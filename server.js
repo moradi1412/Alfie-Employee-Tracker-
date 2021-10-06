460a00ec71e56f89955305a3ad72249707fe696d
Moradi1412 <moradi.javid@gmail.com>
2021-10-06 01:06:33 -0700

    added view all employee by department

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const inquirer = require('inquirer');
 const cTable = require('console.table');
 
 
-
+console.log( "-----------------------------------------------------"); 
+console.log( "------------------EMPLOYEE TRACKER-------------------"); 
+console.log( "-----------------------------------------------------"); 
 const promptUser = () => {
   return inquirer.prompt(
     { 
@@ -22,6 +24,13 @@ const promptUser = () => {
       console.log(answer.choices); 
       if (answer = 'view all employees') { 
         db.query ("SELECT * FROM employee", function (err, res) {
+          if (err) 
+          throw err 
+          console.table(res); 
+        })
+      }
+      if (answer = 'view all employees by department') { 
+        db.query ("SELECT employee.*, department.name AS Department_Name FROM employee LEFT JOIN department ON employee.role_id = department.id", function (err, res) {
           if (err) 
           throw err 
           console.table(res); 
