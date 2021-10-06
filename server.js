050369604822461b89d9e178caf5fbe55cbd8e30
Moradi1412 <moradi.javid@gmail.com>
2021-10-06 01:43:37 -0700

    added queries for role and employee by role

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ const promptUser = () => {
       choices: ['view all employees', 
                 'view all employees by department',
                 'view all roles within each department',
-                'view all employees by Manager',                 
+                'view all the role',                 
+                'view all employees by roles',                 
                 'Add employee', 
                 'Remove an employee', 
                 'update employee role',  
@@ -39,6 +40,20 @@ const promptUser = () => {
       }
       if (answer = 'view all roles within each department') {
         db.query ("SELECT * FROM employee_role LEFT JOIN department ON employee_role.department_id = department.id", function (err, res) {
+          if (err) 
+          throw err 
+          console.table(res); 
+        })
+      }
+      if (answer = 'view all employees by roles') {
+        db.query ("SELECT employee.*, employee_role.title AS Title FROM employee LEFT JOIN employee_role ON employee.role_id = employee_role.id", function (err, res) {
+          if (err) 
+          throw err 
+          console.table(res); 
+        })
+      }
+      if (answer = 'view all the role ') {
+        db.query ("SELECT * FROM employee_role", function (err, res) {
           if (err) 
           throw err 
           console.table(res); 
